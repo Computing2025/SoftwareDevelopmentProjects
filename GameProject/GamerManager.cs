81cf2436d257ffac1686d683e801524a7ecde1cb
Computing2025 <ozk8000@gmail.com>
2024-02-05 00:54:38 +0300

    Versiyon 1.0

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    internal class GamerManager : IGamerService
+    {
+        public void Add(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt eklendi.");
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt silindi.");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt güncellendi.");
+        }
+    }
+}
+
+
